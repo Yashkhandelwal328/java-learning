8cb07c88d572bffc87c4cbfe583da342321fdd09
Yashkhandelwal328 <yashkhandeelwa@gmail.com>
2026-02-18 07:02:43 +0000

    added arrayOfObjects in learning with car1 class

---
+++ learning/OOPS/Car1.java
@@ -0,0 +1,15 @@
+package learning.OOPS;
+// This one is for array of objects
+public class Car1 {
+    String model;
+    String color;
+
+    Car1(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    void drive(){
+        System.out.println("You drive the " + this.color + " " + this.model);
+    }
+}
---
+++ learning/OOPS/arrayOfObjects.java
@@ -0,0 +1,20 @@
+package learning.OOPS;
+
+public class arrayOfObjects {
+    public static void main(String[] args) {
+        Car1 car1 = new Car1("Mustang", "Red");
+        Car1 car2 = new Car1("Corvette", "Blue");
+        Car1 car3 = new Car1("Charger", "Yellow");
+
+        // Car[] cars = new Car[3];
+        Car1[] cars = {car1,car2,car3};
+
+        // for(int i = 0; i < cars.length; i++){
+        //     cars[i].drive();
+        // }
+
+        for(Car1 car : cars){
+            car.drive();
+        }
+    }
+}
